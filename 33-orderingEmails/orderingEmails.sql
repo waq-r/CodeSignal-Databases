431c94048d1da1a53a774f8f2a2e235cb6bb40b6
Vaqar <waqar3@gmail.com>
2019-10-08 20:04:05 +0100

    Update orderingEmails.sql

--- 33-orderingEmails/orderingEmails.sql
+++ 33-orderingEmails/orderingEmails.sql
@@ -1,12 +1,11 @@
 CREATE PROCEDURE orderingEmails()
 BEGIN
-
-	SELECT id, email_title,
+SELECT id, email_title,
     CASE
     WHEN size >= 1048576
     THEN CONCAT(round(floor(size/POW(2,20))),' ','Mb')
     ELSE CONCAT(round(floor(size/POW(2,10))),' ','Kb')
     END as short_size
-    FROM emails
-    ORDER BY size DESC;
+FROM emails
+ORDER BY size DESC;
 END
