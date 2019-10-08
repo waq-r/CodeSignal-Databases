036f51562c775647ec6563c9df255d339ab5ccf2
Vaqar <waqar3@gmail.com>
2019-10-08 20:03:04 +0100

    Ordering Emails SQL

---
+++ 33-orderingEmails/orderingEmails.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE orderingEmails()
+BEGIN
+
+	SELECT id, email_title,
+    CASE
+    WHEN size >= 1048576
+    THEN CONCAT(round(floor(size/POW(2,20))),' ','Mb')
+    ELSE CONCAT(round(floor(size/POW(2,10))),' ','Kb')
+    END as short_size
+    FROM emails
+    ORDER BY size DESC;
+END
