7b34185380cfec50245732cde475f674ef7c15c5
Vaqar <waqar3@gmail.com>
2019-11-02 21:55:58 +0000

    Update validPhoneNumbers.sql

---
+++ 37-validPhoneNumbers/validPhoneNumbers.sql
@@ -0,0 +1,7 @@
+CREATE PROCEDURE validPhoneNumbers()
+BEGIN
+	SELECT *
+    FROM phone_numbers
+    WHERE phone_number REGEXP '^((1-)|\\(1\\))[0-9]{3}-[0-9]{3}-[0-9]{4}$'
+    ORDER BY surname;
+END
