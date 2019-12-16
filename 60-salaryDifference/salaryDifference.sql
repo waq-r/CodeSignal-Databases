5ebc732637826cb26f4637352f73e6d799b3cc88
Vaqar <waqar3@gmail.com>
2019-12-16 23:34:25 +0500

    Update salaryDifference.sql

--- 60-salaryDifference/salaryDifference.sql
+++ 60-salaryDifference/salaryDifference.sql
@@ -2,7 +2,7 @@ CREATE PROCEDURE salaryDifference()
 BEGIN
     SET @maxSal = (SELECT max(salary) FROM employees);
     SET @minSal = (SELECT min(salary) FROM employees);
-	  SET @maxSum = (SELECT @maxSal * COUNT(*) FROM employees WHERE salary =@maxSal);
+	SET @maxSum = (SELECT @maxSal * COUNT(*) FROM employees WHERE salary =@maxSal);
     SET @minSum = (SELECT @minSal * COUNT(*) FROM employees WHERE salary =@minSal);
     
     SELECT IFNULL(@maxSum - @minSum, 0) as difference;
